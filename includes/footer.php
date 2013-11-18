e6749cb2b5feb8c086ca8b6ef37cec8114e60494
Pyrrhate <guillaume.canva@gmail.com>
2013-11-18 15:30:10 +0100

    new 2

---
+++ includes/footer.php
@@ -0,0 +1,49 @@
+<section id="footer">
+  <div class="container">
+    <div class="separator margin-xs"></div>
+    <div class="col-md-3">
+      <ul class="list-unstyled">
+        <li class="list-group-item panel-heading">
+          <h3 class="panel-title">Lorem Ipsum</h3>
+        </li>
+        <li class="list-group-item"><a href="#">Lorem ipsum</a></li>
+        <li class="list-group-item"><a href="#">Lorem ipsum</a></li>
+        <li class="list-group-item"><a href="#">Lorem ipsum</a></li>
+        <li class="list-group-item"><a href="#">Lorem ipsum</a></li>
+      </ul>
+    </div>
+    <div class="col-md-3">
+      <ul class="list-unstyled">
+        <li class="list-group-item panel-heading">
+          <h3 class="panel-title">Lorem Ipsum</h3>
+        </li>
+        <li class="list-group-item"><a href="#">Lorem ipsum</a></li>
+        <li class="list-group-item"><a href="#">Lorem ipsum</a></li>
+        <li class="list-group-item"><a href="#">Lorem ipsum</a></li>
+        <li class="list-group-item"><a href="#">Lorem ipsum</a></li>
+      </ul>
+    </div>
+    <div class="col-md-3">
+      <ul class="list-unstyled">
+        <li class="list-group-item panel-heading">
+          <h3 class="panel-title">Lorem Ipsum</h3>
+        </li>
+        <li class="list-group-item"><a href="#">Lorem ipsum</a></li>
+        <li class="list-group-item"><a href="#">Lorem ipsum</a></li>
+        <li class="list-group-item"><a href="#">Lorem ipsum</a></li>
+        <li class="list-group-item"><a href="#">Lorem ipsum</a></li>
+      </ul>
+    </div>
+    <div class="col-md-3">
+      <ul class="list-unstyled">
+        <li class="list-group-item panel-heading">
+          <h3 class="panel-title">Lorem Ipsum</h3>
+        </li>
+        <li class="list-group-item"><a href="#">Lorem ipsum</a></li>
+        <li class="list-group-item"><a href="#">Lorem ipsum</a></li>
+        <li class="list-group-item"><a href="#">Lorem ipsum</a></li>
+        <li class="list-group-item"><a href="#">Lorem ipsum</a></li>
+      </ul>
+    </div>
+  </div>
+</section>
--- index.php
+++ index.php
@@ -13,7 +13,9 @@
 </head>
 <body>
   <a class="sr-only" href="#content">Skip to main content</a>
+  
 	<?php require_once('includes/nav.php');?>
+  
 	<section id="home-header" class="home-page">
   	<div class="container">
   		<div class="jumbotron">
@@ -23,148 +25,27 @@
       </div>
   	</div>
   </section>
-  <section class="navbar navbar-default home-page" id="clients-banner">
-      <div class="container">
-        <ul class="row">
-          <li class="col-sm-6 col-md-3">
-            <a href="#" class="thumbnail partner">
-              <img class="" data-src="holder.js/100%x180" alt="..." src="data/pictures/">
-            </a>
-          </li>
-          <li class="col-sm-6 col-md-3">
-            <a href="#" class="thumbnail partner">
-              <img class="" data-src="holder.js/100%x180" alt="..." src="data/pictures/">
-            </a>
-          </li>
-          <li class="col-sm-6 col-md-3">
-            <a href="#" class="thumbnail partner">
-              <img class="" data-src="holder.js/100%x180" alt="..." src="data/pictures/">
-            </a>
-          </li>
-          <li class="col-sm-6 col-md-3">
-            <a href="#" class="thumbnail partner">
-              <img class="" data-src="holder.js/100%x180" alt="..." src="data/pictures/">
-            </a>
-          </li>
-        </ul>
-      <div>
-  </section>
-
-  <section id="home-slide" class="home-page">
-      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-        </ol>
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner">
-          <div class="item active">
-            <img src="data/pictures/01.png" alt="...">
-            <div class="carousel-caption">
-              <h3>picture 1</h3>
-            </div>
-          </div>
-          <div class="item">
-            <img src="data/pictures/02.png" alt="...">
-            <div class="carousel-caption">
-              <h3>picture 2</h3>
-            </div>
-          </div>
-          <div class="item">
-            <img src="data/pictures/03.png" alt="...">
-            <div class="carousel-caption">
-              <h3>picture 3</h3>
-            </div>
-          </div>
-        </div>
-        <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left"></span>
-        </a>
-        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right"></span>
-        </a>
+  
+  <?php require_once('includes/home-clients-banner.php'); ?>
+  <?php require_once('includes/home-slide.php'); ?>
+  <?php require_once('includes/home-services.php'); ?>
    
-      </div>
-  </section>
-  <section id="home-services">
+  <section class="margin-xs">
     <div class="container">
-      <div class="page-header text-center">
-        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit !</p>
-      </div>
-        <div class="row">
-          <div class="col-md-4">
-            <div class="glyphicon glyphicon-globe"></div>
-            <div class="caption">
-              <h3>Lorem ipsum</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, doloremque eaque possimus delectus dignissimos accusamus ipsam aut illo. Tenetur, odit deleniti minima velit aperiam ut obcaecati consectetur voluptatum beatae explicabo.</p>
-              <p>
-                <a href="#">Read More</a> 
-              </p>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="glyphicon glyphicon-cog"></div>
-            <div class="caption">
-              <h3>Lorem ipsum</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, quaerat perspiciatis incidunt recusandae architecto ut iusto totam excepturi laudantium. Deserunt, officiis blanditiis ea impedit asperiores laborum eveniet aliquid numquam perferendis.</p>
-              <p>
-                <a href="#">Read More</a> 
-              </p>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="glyphicon glyphicon-pencil"></div>
-            <div class="caption">
-              <h3>Lorem ipsum</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, ipsum qui molestiae voluptates hic tenetur laboriosam assumenda magni dolor id? Voluptates, quaerat atque saepe pariatur tempore ipsam eveniet obcaecati blanditiis?</p>
-              <p>
-                <a href="#">Read More</a> 
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-4">
-            <div class="glyphicon glyphicon-file"></div>
-            <div class="caption">
-              <h3>Lorem ipsum</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, reiciendis, nobis vel nesciunt praesentium voluptatem recusandae sint possimus ex amet consectetur quidem exercitationem labore blanditiis iste maiores rerum minus doloremque.</p>
-              <p>
-                <a href="#">Read More</a> 
-              </p>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="glyphicon glyphicon-folder-open"></div>
-            <div class="caption">
-              <h3>Lorem ipsum</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, corrupti eligendi ea iste nulla placeat aperiam maxime. Quisquam, cumque error est amet recusandae neque harum distinctio laudantium. Nesciunt, optio, quasi.</p>
-              <p>
-                <a href="#">Read More</a> 
-              </p>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="glyphicon glyphicon-briefcase"></div>
-            <div class="caption">
-              <h3>Lorem ipsum</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, ipsa, at repellat ducimus officiis omnis consectetur ea nesciunt alias inventore eaque quibusdam nostrum tenetur nam provident voluptatibus sequi ratione atque.</p>
-              <p>
-                <a href="#">Read More</a> 
-              </p>
-            </div>
-          </div>
-      </div>
-      <div class="panel panel-default">
+      <div class="panel panel-default margin-xs">
         <div class="panel-body text-center">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit.
         </div>
       </div>
+      <div class="text-left margin-xs">
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit !</p>
+        <a class="btn btn-primary btn-warning btn-lg" href="#">S'inscrire</a>
+      </div>
+    </div>
   </section>
-
+  
+  <?php require_once('includes/footer.php'); ?>
+  
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
   <script src="js/bootstrap.js"></script>
 	<script src="js/custom/main.js"></script>
